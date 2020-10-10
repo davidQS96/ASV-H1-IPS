0b98ab19189ca4776d5c29a4d60519db40e6f1fe
davidQS96 <36705742+davidQS96@users.noreply.github.com>
2020-10-10 01:16:33 -0600

    Commit gran parte de interfaz inicial en Main.py
    +Se logra ver la ventana principal de aplicacion
    +Se puede cargar un directorio de imagen
    +Revisa si es tipo imagen (tiene soporte de algunos formatos)
    +Se puede visualizar una imagen (no corresponde a imagen seleccionada)
    
    -browseImgFile aun no muestra mensaje de error en interfaz
    -pantalla inicial aun no muestra tamaño
    -no se ha completado la clase CurrentState
    -Solo se colocaron links donde se obtuvo codigo, no que son
    -Interfaz inicial se debe ordenar
    -se debe seguir con clasifScreen
    -aun no muestra algunos detalles de imagen al cargarla

--- Main.py
+++ Main.py
@@ -1,7 +1,94 @@
-for i in range(50):
-    print(i + 4)
-    print(i + 3)
+#Bibliotecas
+from tkinter import * #Para GUI (filedialog, etc)
+from tkinter import filedialog #Manejo de archivos
+from PIL import ImageTk, Image #Manejo de imágenes
+import os
 
-    print(i - 5)
+#-------------------------------------------------------
+#Constantes
 
-    print(2*i)
+#-------------------------------------------------------
+#Funciones
+
+#Función que muestra al usuario una ventana de búsqueda de archivos y asigna a resultStr el directorio donde se encuentra una imagen válida
+#resultStr es un tkinter.StringVar()
+def browseImgFile(resultStr):
+    
+    #https://stackoverflow.com/questions/19944712/browse-for-file-path-in-python
+    currdir = os.getcwd()
+
+    #https://docs.python.org/3.9/library/dialog.html
+    tempdir = filedialog.askopenfilename(parent=root, initialdir = currdir, title = 'Seleccione un archivo de imagen') #Ventana emergente
+
+    #https://www.thetopsites.net/article/53470882.shtml
+    if len(tempdir) > 0 and tempdir.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')): #Archivos de imagen soportados
+        resultStr.set(tempdir) #Asigna la imagen válida que se encontró
+
+    else:
+        print("Archivo no valido")        
+
+    return
+
+#-------------------------------------------------------
+#Clases
+
+class CurrentState:
+
+    def __init__(self):
+        self.currRelImgPath = 2
+
+
+
+
+
+
+#-------------------------------------------------------
+#Programa principal
+
+
+##Mostrar tamano, 
+def mainScreen():
+    #https://www.youtube.com/watch?v=YXPyB4XeYLA&ab_channel=freeCodeCamp.org
+    titleLbl = Label(root, text = "Tarea 1 - Principios de utilización del color")
+    pathStrVar = StringVar()
+
+    filePathLbl = Label(root, textvariable = pathStrVar)
+    browseBtn = Button(root, text = "Buscar imagen", command = lambda: browseImgFile(pathStrVar))
+    nextBtn = Button(root, text = "Siguiente")
+
+    #http://effbot.org/pyfaq/why-do-my-tkinter-images-not-appear.htm
+    imagePI = ImageTk.PhotoImage(Image.open("Imágenes Prueba/paisajeBW2.jpg"))
+    imageLbl = Label(image = imagePI)
+    imageLbl.image = imagePI # keep a reference!
+    
+
+    titleLbl.pack()
+    imageLbl.pack()
+    filePathLbl.pack()
+    browseBtn.pack()
+    nextBtn.pack()
+
+    
+
+def clasifScreen():
+    return
+    
+    
+
+root = Tk()
+mainScreen()
+root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
