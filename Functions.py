bb9c2ba4ea48b6b7a6e4eefeeb9db051828f9bff
ema108 <72424177+ema108@users.noreply.github.com>
2020-10-11 19:25:58 -0600

    agregada función filtro sal y pimienta
    se agrega función filtrosalpimmienta, descripción en el documento se corrigue error en la importar

--- Functions.py
+++ Functions.py
@@ -5,7 +5,8 @@ import matplotlib.image as mpimg
 from matplotlib import pyplot as plt
 import numpy
 import math
-
+from skimage.filters.rank import median
+from skimage.morphology import disk
 
 """
 importar requiere el nombre del archivo como string
@@ -31,4 +32,40 @@ def importar(direccion):
         grey = igual.all()
         if grey == True:
             img = red
+            color = False
     return img, color, dimen
+
+
+"""
+filtrosalpimienta 
+entradas
+imagen: imagen que se desea modificar color o grises
+color: valor booleano, true si es a color o false si es grises
+intensidad: valor booleano, true para elevado o false para moderado
+crojo,cverde,cazul: valor booleano, opcional, true si se desea modificar un canal
+
+la función retorna la imagen filtrado
+
+"""
+def filtrosalpimienta(imagen,color,intensidad,crojo=None,cverde=None,cazul=None):
+    if intensidad == True:
+        dureza = 3
+    else:
+        dureza = 1
+    if color == True:
+        red = imagen[:,:,0]
+        green = imagen[:,:,1]
+        blue = imagen[:,:,2]
+        final = imagen
+        if crojo == True:
+            finalr = median(red,disk(dureza)) 
+            final[:,:,0] = finalr
+        if cverde == True:
+            finalg = median(green,disk(dureza)) 
+            final[:,:,1] = finalg
+        if cazul == True:
+            finalb = median(blue,disk(dureza))  
+            final[:,:,2] = finalb
+    else:
+        final = median(imagen,disk(dureza))
+    return final
