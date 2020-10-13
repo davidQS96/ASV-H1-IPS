dab0e0ca4a01604ab0e96e31e18a10403118af60
ema108 <72424177+ema108@users.noreply.github.com>
2020-10-12 20:50:51 -0600

    Agrega función filtro gaussiano
    Se agrega la función de filtro gaussiano, descripción en el codigo

--- Functions.py
+++ Functions.py
@@ -7,7 +7,7 @@ import numpy
 import math
 from skimage.filters.rank import median
 from skimage.morphology import disk
-
+from skimage.morphology import diamond
 """
 importar requiere el nombre del archivo como string
 
@@ -79,4 +79,51 @@ def filtrosalpimienta(imagen,rgb,intensidad,unsolocolor,roj=None,ver=None,azu=No
             final = fil
     else:
         final = median(imagen,disk(dureza))
+    return final
+
+
+"""
+filtrogaussiano
+entradas
+imagen: imagen que se desea modificar color o grises
+color: valor booleano, true si es a color o false si es grises
+intensidad: valor booleano, true para elevado o false para moderado
+unsolocolor: valor booleano, true para filtrar en un solo color; necesita los valores roj,ver,azu
+roj,ver,azu: valor de color en cada canal RGB
+
+la función retorna la imagen filtrado
+
+"""
+
+def gaussiano(imagen,rgb,intensidad,unsolocolor,roj=None,ver=None,azu=None):
+    if intensidad == True:
+        dureza = 5
+    else:
+        dureza = 3
+    if rgb == True:
+        fil = imagen.copy()
+        cont = 0
+        while cont < 3:
+            fil[:,:,cont] = median(imagen[:,:,cont],diamond(dureza))
+            cont = cont +1
+        if unsolocolor == True:
+          orig = imagen.copy()
+          red = imagen[:,:,0] == roj
+          green = imagen[:,:,1] == ver
+          blue = imagen[:,:,2] == azu
+          parte = red == green
+          parte = parte == blue
+          contraparte = ~parte 
+          seccion = fil.copy()
+          resto = orig.copy()
+          a = 0
+          while a < 3:
+              seccion[:,:,a]=seccion[:,:,a]*parte
+              resto[:,:,a]=resto[:,:,a]*contraparte
+              a=a+1
+          final = resto+seccion
+        else:
+            final = fil
+    else:
+        final = median(imagen,diamond(dureza))
     return final
